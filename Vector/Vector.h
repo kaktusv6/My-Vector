cd2c04d2a3ac87873d67b8f640e07ef90770b049
Vasiliy <kaktusv6@gmail.com>
2016-05-11 15:07:26 +1000

    Add methods in class Vector. Define constructors class Vector.

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -4,25 +4,31 @@
 template<typename T>
 class Vector
 {
-	int size;
+	int sizeArray;
 public:
 	T* array;
 
 	/* --------------- Class constructors --------------- */
 
-	Vector();
-	Vector(int _size);
-	Vector(int _size, T value);
-	Vector(T* _array);
+	Vector() {}
+	Vector(int);
+	Vector(int, T);
+	Vector(T*);
 
 	/* --------------- Class methods --------------- */
 
+	void swap(Vector<T>);
+	int size();
 	bool empty() const;
-	T& front();
-	T& back();
-	T& at();
+	T* begin();
+	T* end();
 	Vector& pushBack(T);
 	Vector& popBack();
+	Vector& clear();
+	Vector& insert(T*, T);
+	Vector& erase(T*);
+	Vector& erase(T*, int);
+	Vector& erase(T*, T*);
 
 	/* --------------- Class operators --------------- */
 
@@ -30,6 +36,29 @@ public:
 
 };
 
+/* --------------- Class Vectro methods and operators --------------- */
+
+template<typename T>
+Vector<T>::Vector(int _size)
+{
+	array = new T[_size];
+	sizeArray = _size;
+}
+
+template<typename T>
+Vector<T>::Vector(int _size, T value)
+{
+	array = new T[_size];
+	sizeArray = _size;
+	for(int i = 0; i < _size; i++)
+		array[i] = value;
+}
+
+template<typename T>
+Vector<T>::Vector(T* _array)
+{
+	array = _array;
+}
 class Range{
 public:
 	Range() {}
