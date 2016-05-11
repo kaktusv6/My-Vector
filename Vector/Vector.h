5532474bcaf7d2ef09e8d2d8dcc75b3b14cceee1
Vasiliy <kaktusv6@gmail.com>
2016-05-11 15:35:09 +1000

    Add destructor in class String and definr method pushBack.

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -1,6 +1,8 @@
 #ifndef VECTOR_VECTOR_H
 #define VECTOR_VECTOR_H
 
+#include <stddef.h>
+
 template<typename T>
 class Vector
 {
@@ -10,7 +12,7 @@ public:
 
 	/* --------------- Class constructors --------------- */
 
-	Vector() {}
+	Vector();
 	Vector(int);
 	Vector(int, T);
 	Vector(T*);
@@ -34,10 +36,23 @@ public:
 
 	T& operator[] (int);
 
+	/* --------------- Class Destructor --------------- */
+
+	~Vector()
+	{
+		delete[] array;
+	}
+
 };
 
 /* --------------- Class Vectro methods and operators --------------- */
 
+template<typename T>
+Vector<T>::Vector()
+{
+	array = NULL;
+	sizeArray = 0;
+}
 template<typename T>
 Vector<T>::Vector(int _size)
 {
@@ -59,6 +74,30 @@ Vector<T>::Vector(T* _array)
 {
 	array = _array;
 }
+
+template<typename T>
+Vector<T>& Vector<T>::pushBack(T value)
+{
+	if (array == NULL)
+	{
+		array = new int[1];
+		array[0] = value;
+		sizeArray++;
+	}
+	else
+	{
+		T *_array = new T[sizeArray + 1];
+		for(int i = 0; i < sizeArray; i++)
+			_array[i] = array[i];
+
+		_array[sizeArray] = value;
+		delete[] array;
+		array = _array;
+		sizeArray++;
+	}
+	return *this;
+}
+
 class Range{
 public:
 	Range() {}
