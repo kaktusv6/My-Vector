02e1192f5bf9961b9360741700a655b8d5e0e3ea
Vasiliy <kaktusv6@gmail.com>
2016-05-24 23:32:41 +1000

    Add tests for meyhod pushBackin class Vector.

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 class VectorTest : public ::testing::Test
 {
-	vector<int> *v_int = NULL;
-	vector<char> *v_char = NULL;
+protected:
+
+	Vector<int> *v_int = NULL;
+	Vector<char> *v_char = NULL;
 
 	void SetUp()
 	{
-		v_int = new vector<int>();
-		v_char = new vector<char>();
+		v_int = new Vector<int>();
+		v_char = new Vector<char>();
 
 	}
 
@@ -23,8 +25,33 @@ class VectorTest : public ::testing::Test
 	}
 };
 
-int main()
+TEST_F(VectorTest, TestPushBack)
 {
+	int a[8] = {
+		2, 3, 24, 2423, 9345, 526, 0, 0
+	};
+	v_int->pushBack(2)
+		.pushBack(3)
+		.pushBack(24)
+		.pushBack(2423);
 
+	ASSERT_EQ(4, v_int->size());
+	for(int i = 0; i < v_int->size(); i++){
+		ASSERT_EQ(a[i], (*v_int)[i]);
+	}
+	v_int->pushBack(9345)
+		.pushBack(526)
+		.pushBack(0)
+		.pushBack(0);
+
+	ASSERT_EQ(8, v_int->size());
+	for(int i = 0; i < v_int->size(); i++){
+		ASSERT_EQ(a[i], (*v_int)[i]);
+	}
+}
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
 	return 0;
 }
